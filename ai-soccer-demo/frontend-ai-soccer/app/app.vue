4ef374c118c067b1e46e21544a838c74c030d960
Martin Bäumer <martin.baeumer@gmail.com>
2026-03-25 00:44:55 +0100

    Set a shorter polling interval

--- ai-soccer-demo/frontend-ai-soccer/app/app.vue
+++ ai-soccer-demo/frontend-ai-soccer/app/app.vue
@@ -18,7 +18,7 @@ onMounted(() => {
         console.error(e)
       }
 
-      await new Promise(resolve => setTimeout(resolve, 1000))
+      await new Promise(resolve => setTimeout(resolve, 750))
     }
   }
 
